68cec652f9d75ceb444a51780c7e6ae8030c02b5
yashkaf <yashkaf@gmail.com>
2015-09-22 16:59:35 -0400

    Almost complete draft of Reg Mod project Rmd

--- class_project.R
+++ class_project.R
@@ -1,9 +1,18 @@
 library(datasets)
 library(ggplot2)
+library(reshape2)
 data(mtcars)
 g=ggplot(data=mtcars)
 
-#Exploratory:
+# Exploratory:
 summary(lm(mpg~am,data=mtcars)) #Significant so probably connection exists (unlikely that cofounders nullify)
-g+geom_boxplot(aes(factor(am),mpg,fill=factor(am)))+labs(title="MPG for manual automatic cars",x="Trasmission", y="MPG")+theme(legend.position="none")+scale_x_discrete(labels=c("Automatic","Manual"))
+ggplot(data=mtcars)+geom_boxplot(aes(factor(am),mpg,fill=factor(am)))+labs(title="MPG for manual automatic cars",x="Trasmission", y="MPG")+theme(legend.position="none")+scale_x_discrete(labels=c("Automatic","Manual"))
 
+# Naive model
+fit1=lm(mpg~am,data=mtcars)
+summary(fit1)
+
+# Possible confounders
+cormt=cor(mtcars)
+melmt=melt(cormt[,c(1,9)])
+ggplot(data=melmt,aes(x=Var1,y=Var2,fill=value)) + geom_tile(color="black") + scale_fill_gradient2(low="brown3",high="chartreuse3",mid="white",limit=c(-1,1))+geom_text(aes(Var1,Var2,label=round(value,2)),size=4,color="black")+labs(title="MPG and Transmission Covariates", x="",y="Covariates",fill="Correlation")
